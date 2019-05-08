9608e41ab137b4389636e0082bd1fe438fc21688
Priya Ananthasankar <prananth@microsoft.com>
2019-05-07 17:45:49 -0700

    install package changes

--- KafkaTwitterTrigger/main.py
+++ KafkaTwitterTrigger/main.py
@@ -1,10 +1,23 @@
+import logging,json
 from azure_functions.kafka import KafkaEvent
-import logging
+from textblob import TextBlob
 
 def main(kevent : KafkaEvent):
+    # KafkaEvent rich type
     logging.info(kevent.get_body().decode('utf-8'))
     logging.info(kevent.key)
     logging.info(kevent.offset)
     logging.info(kevent.partition)
     logging.info(kevent.topic)
     logging.info(kevent.timestamp)
+
+    # Sentiment Analysis
+    msg = json.loads(kevent.get_body().decode('utf-8'))
+    logging.info(msg["Value"])
+    testimonial = TextBlob(msg["Value"])
+    logging.info(testimonial.sentiment)
+    if testimonial.sentiment.polarity < 0:
+        logging.info("Negative")
+    else:
+        logging.info("Positive")
+
